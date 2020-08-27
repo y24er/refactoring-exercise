004dfb144fdd3bb66021e271dd1f4e06ffe17e58
Jessie <1028704486@qq.com>
2020-08-27 15:34:10 +0800

    [statement][Jessie] refactor: extract printResult method

--- src/statement.js
+++ src/statement.js
@@ -1,31 +1,42 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+  let orderContent = '';
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
     thisAmount = calculateAmount(play, thisAmount, perf);
     let thisCredit = 0;
     thisCredit = calculateCredit(perf, play);
-    result += ` ${play.name}: ${format(thisAmount)} (${perf.audience} seats)\n`;
+    orderContent += printOrderLine(play, thisAmount, perf);
     totalAmount += thisAmount;
     volumeCredits += thisCredit;
   }
-  result += `Amount owed is ${format(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits \n`;
+  let result = printResult(invoice, orderContent, totalAmount, volumeCredits);
   return result;
 }
 
 module.exports = {
   statement,
 };
+function printResult(invoice, orderContent, totalAmount, volumeCredits) {
+  let result = `Statement for ${invoice.customer}\n`;
+  result += orderContent;
+  result += `Amount owed is ${format(totalAmount)}\n`;
+  result += `You earned ${volumeCredits} credits \n`;
+  return result;
+}
+
+function printOrderLine(play, thisAmount, perf) {
+  return ` ${play.name}: ${format(thisAmount)} (${perf.audience} seats)\n`;
+}
+
 function format(amount) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
-  }).format(amount/100);
+  }).format(amount / 100);
 }
 
 function calculateCredit(perf, play) {
